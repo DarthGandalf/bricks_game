c616a8291f72159f6f03945b033c282cb4bc9b23
Alina <alinazhiltsova@gmail.com>
2018-02-26 21:46:32 +0000

    2 files

--- game.js
+++ game.js
@@ -1,106 +1,106 @@
 var ballX = 75;
-var ballY = 75;
-var ballSpeedX = 5;
-var ballSpeedY = 7;
+	var ballY = 75;
+	var ballSpeedX = 5;
+	var ballSpeedY = 7;
 
-const BRICK_W = 80;
-const BRICK_H = 20;
-const BRICK_GAP = 2;
-const BRICK_COLS = 10;
-const BRICK_ROWS = 14; 
-var brickGrid = new Array(BRICK_COLS * BRICK_ROWS);
-var bricksLeft = 0;
+	const BRICK_W = 80;
+	const BRICK_H = 20;
+	const BRICK_GAP = 2;
+	const BRICK_COLS = 10;
+	const BRICK_ROWS = 14; 
+	var brickGrid = new Array(BRICK_COLS * BRICK_ROWS);
+	var bricksLeft = 0;
 
-const PADDLE_WIDTH = 100;
-const PADDLE_THICKNESS = 10;
-const PADDLE_DIST_FROM_EDGE = 60;
-var paddleX = 400;
+	const PADDLE_WIDTH = 100;
+	const PADDLE_THICKNESS = 10;
+	const PADDLE_DIST_FROM_EDGE = 60;
+	var paddleX = 400;
 
-var canvas, canvasContext;
+	var canvas, canvasContext;
 
-var mouseX =0;
-var mouseY =0;
+	var mouseX =0;
+	var mouseY =0;
 
-function updateMousePos(evt) {
-	var rect = canvas.getBoundingClientRect();
-	var root = document.documentElement;
+	function updateMousePos(evt) {
+		var rect = canvas.getBoundingClientRect();
+		var root = document.documentElement;
 
-	mouseX = evt.clientX - rect.left - root.scrollLeft;
-	mouseY = evt.clientY - rect.top - root.scrollTop;
-	paddleX = mouseX - PADDLE_WIDTH/2;
-}
-
-function brickReset() {
-	bricksLeft = 0;
-	var i;
-	for(var i=0; i< 3*BRICK_COLS; i++) {
-	brickGrid[i] = false;
-}
-	for(;i<BRICK_COLS*BRICK_ROWS;i++) {
-	brickGrid[i] = true;
-	bricksLeft ++;
+		mouseX = evt.clientX - rect.left - root.scrollLeft;
+	    mouseY = evt.clientY - rect.top - root.scrollTop;
+		paddleX = mouseX - PADDLE_WIDTH/2;
 	}
-}
 
-window.onload = function(){
-	canvas = document.getElementById('gameCanvas');
-	canvasContext = canvas.getContext('2d');
-
-	var framesPerSecond = 30;
-	setInterval(updateAll, 1000/framesPerSecond);
-
-	canvas.addEventListener('mousemove', updateMousePos);
-
-	brickReset();
-	ballReset();
-}
-
-function updateAll() {
-	moveAll();
-	drawAll();
-}
-
-function ballReset(){
-	ballX = canvas.width/2;
-	ballY = canvas.height/2;
-}
-
-function ballMove (){
-    ballX+= ballSpeedX;
-	ballY+= ballSpeedY;
-
-	if(ballX < 0 && ballSpeedX <0.0){  //left
-	ballSpeedX *= -1;
-
-	if(ballX > canvas.width && ballSpeedX >0.0){ //right
-	ballSpeedX *= -1;
+	function brickReset() {
+		bricksLeft = 0;
+		var i;
+	   for(var i=0; i< 3*BRICK_COLS; i++) {
+	   	brickGrid[i] = false;
+	   }
+	   for(;i<BRICK_COLS*BRICK_ROWS;i++) {
+	   	brickGrid[i] = true;
+	   	bricksLeft ++;
+	   }
 	}
-	if(ballY < 0 && ballSpeedY < 0.0){ //top
-	ballSpeedY *= -1;
 
-    }
-	if(ballY > canvas.height){ //bottom
-		ballReset();
+	window.onload = function(){
+		canvas = document.getElementById('gameCanvas');
+		canvasContext = canvas.getContext('2d');
+
+		var framesPerSecond = 30;
+		setInterval(updateAll, 1000/framesPerSecond);
+
+		canvas.addEventListener('mousemove', updateMousePos);
+
 		brickReset();
+		ballReset();
 	}
-}
 
-function isBrickAtColRow(col,row){
+	function updateAll() {
+		moveAll();
+		drawAll();
+	}
 
-	if(col >=0 && col < BRICK_COLS &&
-		row >=0 && row < BRICK_ROWS){
-    var brickIndexUnderCoord = rowColToArrayIndex(col,row);
-    	return brickGrid [brickIndexUnderCoord];
+	function ballReset(){
+		ballX = canvas.width/2;
+		ballY = canvas.height/2;
+	}
+
+	function ballMove (){
+       	ballX+= ballSpeedX;
+		ballY+= ballSpeedY;
+
+		if(ballX < 0 && ballSpeedX <0.0){  //left
+			ballSpeedX *= -1;
+
+		}
+		if(ballX > canvas.width && ballSpeedX >0.0){ //right
+			ballSpeedX *= -1;
+		}
+		if(ballY < 0 && ballSpeedY < 0.0){ //top
+			ballSpeedY *= -1;
+
+		}
+		if(ballY > canvas.height){ //bottom
+			ballReset();
+			brickReset();
+		}
+	}
+
+	function isBrickAtColRow(col,row){
+
+		if(col >=0 && col < BRICK_COLS &&
+		   row >=0 && row < BRICK_ROWS){
+       var brickIndexUnderCoord = rowColToArrayIndex(col,row);
+       return brickGrid [brickIndexUnderCoord];
 	} else {
 		return false;
 	}
 
 }
-
-function ballBrickHandling (){
-	var ballBrickCol = Math.floor (ballX / BRICK_W);
-	var ballBrickRow = Math.floor (ballY / BRICK_H);
-	var brickIndexUnderBall = rowColToArrayIndex(ballBrickCol, ballBrickRow);
+	function ballBrickHandling (){
+		var ballBrickCol = Math.floor (ballX / BRICK_W);
+		var ballBrickRow = Math.floor (ballY / BRICK_H);
+		var brickIndexUnderBall = rowColToArrayIndex(ballBrickCol, ballBrickRow);
 		
 		if(ballBrickCol >=0 && ballBrickCol < BRICK_COLS &&
 		   ballBrickRow >=0 && ballBrickRow < BRICK_ROWS){
